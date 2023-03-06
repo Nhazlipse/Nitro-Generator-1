ae71242bff309d2e6b03d60e76a3cbea50941ab1
Sobhan <90289153+Sobhan-SRZA@users.noreply.github.com>
2023-03-06 18:25:40 +0330

    Update config.js

--- config.js
+++ config.js
@@ -4,3 +4,12 @@ module.exports = {
     avatar: 'https://cdn.discordapp.com/attachments/866395185289232415/1016747244709498941/download.jpg',//here you need place webhook avatar link(place some image link)
     send: false //this is for sent the valid code with webhook in discord channel.you can set it "true" to sent it or set it "false" to don't sending
 }
+/**
+ * @Info
+ * Bot Coded by Mr.SIN RE#1528 :) | https://dsc.gg/persian-caesar
+ * @Info
+ * Work for Persian Caesar | https://dsc.gg/persian-caesar
+ * @Info
+ * Please Mention Us "Persian Caesar", When Have Problem With Using This Code!
+ * @Info
+ */
